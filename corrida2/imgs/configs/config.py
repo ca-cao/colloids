b5f59371941dfb062223414c37ce0ff0ca63ed0c
cacao <j.crabell62@gmail.com>
2019-05-30 17:01:48 -0500

    diagrams

--- corrida2/imgs/configs/config.py
+++ corrida2/imgs/configs/config.py
@@ -8,7 +8,7 @@ from qtree import *
 from octree import *
 import matplotlib.pyplot as plt
 fname=str(sys.argv[1])
-#path=str(sys.argv[2])
+path=str(sys.argv[2])
 
 # obtener parametros del sistema
 lamb,r,rho=getparam(str(sys.argv[1]))
@@ -30,21 +30,25 @@ pk,npks=pf(dist,2)
 lyrs=lyr(rtz[:,3],pk[:,0],.2)
 
 # parametro de orden para las capas
-#for i in range(len(pk)):
-#    hexord(rtz[int(lyrs[i,0]):int(lyrs[i,1]),3:6],.5*lamb)
+for i in range(len(pk)):
+    hexord(rtz[int(lyrs[i,0]):int(lyrs[i,1]),3:6],.5*lamb)
 #xyzwrite(rtz,fname[:-4]+"hx"+".xyz",6) # escribe todo en un archivo
 
 # calcular promedio de ncoord
-#eta=ncoord(xyz,lamb)
-#nbar=np.dot(eta[:,0],eta[:,1])/float(npart)
+eta=ncoord(xyz,lamb)
+nbar=np.dot(eta[:,0],eta[:,1])/float(npart)
 
-#for i in range(len(pk)):
-#    f=open(path+"lyr"+str(i)+'.dat','a')
-#    avg=np.average(rtz[int(lyrs[i,0]):int(lyrs[i,1]),5])
-#    p_f=pk[i,1]/(pk[i,0]*2*40)
-#    #lambda r_cyl rho hexord r_layer packaging_factor #layers  <ncoord>
-#    f.write('{} {} {} {} {} {} {} {}\n'.format(lamb,r,rho,avg,pk[i,0],p_f,npks,nbar))
-#f.close()
+for i in range(len(pk)):
+    if len(pk)==1:
+        f=1
+    else:
+        f=i
+    f=open(path+"lyr"+str(f)+'.dat','a')
+    avg=np.average(rtz[int(lyrs[i,0]):int(lyrs[i,1]),5])
+    p_f=pk[i,1]/(pk[i,0]*2*40*np.pi) #nparti / areaLayer
+    #lambda r_cyl rho hexord r_layer packaging_factor #layers  <ncoord>
+    f.write('{} {} {} {} {} {} {} {}\n'.format(lamb,r,rho,avg,pk[i,0],p_f,npks,nbar))
+f.close()
 
 #plt.plot(ncoord(xyz,lamb))
 #plt.xlabel('ncoord')
--- corrida2/imgs/configs/phase.py
+++ corrida2/imgs/configs/phase.py
@@ -10,6 +10,7 @@ data=np.loadtxt(sys.argv[1])
 data=fun.qsort(data,0,len(data)-1,1)
 #graficar para cada radio \rho \lambda
 r=data[0,1]
+#       0      1    2     3       4       5              6        7
 #    #lambda r_cyl rho hexord r_layer packaging_factor #layers  <ncoord>
 # separar por radios
 rr=np.zeros(7)
@@ -22,13 +23,33 @@ for i in range(len(data)):
 rr[-1]=len(data)
 rr=[int(i) for i in rr]
 rc('text', usetex=True)
-
+data[:,5]=data[:,5]/np.pi
 for j in range(len(rr)-1):
-    sc=plt.scatter(data[rr[j]:rr[j+1],2],data[rr[j]:rr[j+1],0],c=data[rr[j]:rr[j+1],3],cmap='cool',vmin=0,vmax=1)
-    plt.ylabel(r'\lambda')
-    plt.xlabel(r'\rho')
+    # hexatic
+    sc=plt.scatter(data[rr[j]:rr[j+1],2],data[rr[j]:rr[j+1],0],c=data[rr[j]:rr[j+1],3],cmap='hsv',vmin=0,vmax=1,s=125)
+    plt.ylabel(r'$\lambda$')
+    plt.xlabel(r'$\rho$')
     plt.title("Radius of cylinder = {}".format(data[rr[j],1]))
     cbar=plt.colorbar(sc)
     cbar.ax.set_ylabel('hexatic order parameter '+r'$\psi_6$',rotation=270,labelpad=15)
-    plt.show()
+    plt.savefig("hexatic_{}_layer_r{}.png".format(str(sys.argv[2]),data[rr[j],1]))
+    plt.clf()
+    #ncoord
+    sc=plt.scatter(data[rr[j]:rr[j+1],2],data[rr[j]:rr[j+1],0],c=data[rr[j]:rr[j+1],7],cmap='hsv',s=125)
+    plt.ylabel(r'$\lambda$')
+    plt.xlabel(r'$\rho$')
+    plt.title("Radius of cylinder = {}".format(data[rr[j],1]))
+    cbar=plt.colorbar(sc)
+    cbar.ax.set_ylabel('Coordination number',rotation=270,labelpad=15)
+    plt.savefig("ncoord_{}_layer_r{}.png".format(str(sys.argv[2]),data[rr[j],1]))
+    plt.clf()
+    #packaging
+    sc=plt.scatter(data[rr[j]:rr[j+1],2],data[rr[j]:rr[j+1],0],c=data[rr[j]:rr[j+1],7],cmap='hsv',s=125)
+    plt.ylabel(r'$\lambda$')
+    plt.xlabel(r'$\rho$')
+    plt.title("Radius of cylinder = {}".format(data[rr[j],1]))
+    cbar=plt.colorbar(sc)
+    cbar.ax.set_ylabel('Packaging factor',rotation=270,labelpad=15)
+    plt.savefig("packaging_{}_layer_r{}.png".format(str(sys.argv[2]),data[rr[j],1]))
+    plt.clf()
     
